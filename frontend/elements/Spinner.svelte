625e087be70ff25af78b4f946448a1e0934777c0
Abel Bodogan <bodoganabel@gmail.com>
2024-07-02 22:17:31 +0200

    Added spinner and delay

---
+++ frontend/elements/Spinner.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { ProgressRadial } from '@skeletonlabs/skeleton';
+	export let size = 'w-full';
+	export let stroke = 140;
+</script>
+
+<ProgressRadial
+	stroke={stroke}
+	meter="stroke-primary-500"
+	track="stroke-primary-500/30"
+	width={size}
+/>
