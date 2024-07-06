16a9d041da99d4c38b34485c065581199101be3e
bodoganabel <bodoganabel@gmail.com>
2024-07-06 12:54:11 +0200

    Added headers

--- backend/controllers/headers.ts
+++ backend/controllers/headers.ts
@@ -1,5 +1,5 @@
 
-export function setCookieHeader(headers: Headers, key: string, value: string) {
-    headers.append('Set-Cookie', `${key}=${value}; Path=/; HttpOnly; SameSite=Strict`);
+export function setCookieHeader(headers: Headers, key: string, value: string, path = '/') {
+    headers.append('Set-Cookie', `${key}=${value}; Path=${path}; HttpOnly; SameSite=Strict`);
   }
   
