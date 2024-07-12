154a68b53b232b2b047da521d4ffed67e138bc56
Abel Bodogan <bodoganabel@gmail.com>
2024-07-12 07:32:37 +0200

    Added modal

--- frontend/functionality/modal/ModalRoot.svelte
+++ frontend/functionality/modal/ModalRoot.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
     import { get } from "svelte/store";
-    import { getModalStore, modalClose } from "./modal.store";
+    import { modalClose, modalStore } from "./modal.store";
     import CloseIcon from "../../icons/CloseIcon.svelte";
     import LockClosedIcon from "../../icons/LockClosedIcon.svelte";
+    import { onMount } from "svelte";
 
     const handleKeyDown = (event: { key: string }) => {
         // Close modal on pressing escape key
@@ -12,11 +13,20 @@
             modalClose();
         }
     };
+
+    $: {
+        console.log("getModalStore() from ModalRoot:");
+        console.log($modalStore);
+    }
+
+    onMount(() => {
+        console.log("ModalRootMounted");
+    });
 </script>
 
 <div
     class={`${
-        get(getModalStore()).isVisible ? "" : "hidden"
+        $modalStore.isVisible ? "" : "hidden"
     } modal-backdrop fixed top-0 left-0 right-0 bottom-0 overflow-y-auto bg-surface-backdrop-token z-[999]`}
     data-testid="modal-backdrop"
     style=""
@@ -27,7 +37,7 @@
         style=""
         on:click={(event) => {
             if (
-                get(getModalStore()).isOutclickClose &&
+                $modalStore.isOutclickClose &&
                 event.target === event.currentTarget
             ) {
                 modalClose();
@@ -46,13 +56,13 @@
                 class="modal card p-4 pt-1 w-modal shadow-xl space-y-4 bg-slate-50"
             >
                 <div
-                    class="modal-container flex w-full justify-between items-center"
+                    class="modal-container mt-2 flex w-full justify-between items-center"
                 >
-                    <h2 class="modal-title text-lg">
-                        {@html get(getModalStore()).title}
-                    </h2>
-                    <div class="flex items-center space-x-2">
-                        {#if !get(getModalStore()).isOutclickClose}
+                    <header class="modal-header text-2xl font-bold">
+                        {@html $modalStore.title}
+                    </header>
+                    <div class="flex items-center w-6">
+                        {#if !$modalStore.isOutclickClose}
                             <div class="opacity-20">
                                 <LockClosedIcon />
                             </div>
@@ -62,11 +72,11 @@
                         </button>
                     </div>
                 </div>
-                {#if get(getModalStore()).content}
+                {#if $modalStore.content}
                     <div class="flex flex-col items-center">
                         <svelte:component
-                            this={get(getModalStore()).content}
-                            {...get(getModalStore()).params}
+                            this={$modalStore.content}
+                            {...$modalStore.params}
                         />
                     </div>
                 {/if}
