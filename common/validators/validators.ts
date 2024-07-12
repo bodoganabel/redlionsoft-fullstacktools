68530194bf3cf94590463914c4002a9df47bafb0
bodoganabel <bodoganabel@gmail.com>
2024-07-12 10:11:07 +0200

    Formatted validators

--- common/validators/validators.ts
+++ common/validators/validators.ts
@@ -5,8 +5,8 @@ export const VALIDATOR_IS_EMAIL = (message = 'Invalid email') => (value: string)
 
 export const VALIDATOR_IS_PHONE_INTERNATIONAL = (message = 'Invalid phone') => (value: string) => { if (REGEX_VALID_PHONE_INTERNATIONAL.test(value) === false) return message; }
 
-export const VALIDATOR_MINLENGTH = (minCharacters: number,message = 'Not enough characters') => (value: string) => { if (value.length < minCharacters) return message;}
+export const VALIDATOR_MINLENGTH = (minCharacters: number, message = 'Not enough characters') => (value: string) => { if (value.length < minCharacters) return message; }
 
-export const VALIDATOR_REQUIRE_STRING = (message = "Required field") => (value: string, ) => {
+export const VALIDATOR_REQUIRE_STRING = (message = "Required field") => (value: string,) => {
     if (value === '') return message;
 }
