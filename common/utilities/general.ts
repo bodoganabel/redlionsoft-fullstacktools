bd9e99562b5b64390c1ab4a1fd03aa5ca49baa5e
Abel Bodogan <bodoganabel@gmail.com>
2024-10-30 00:58:58 +0100

    Added dumb uuid generator

--- common/utilities/general.ts
+++ common/utilities/general.ts
@@ -1,3 +1,8 @@
 export function delay(ms: number): Promise<void> {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+export function UuidSimple() {
+    const time = new Date().getTime();
+    return time.toString();
+}
