d41b4c91e8a8e63b7c48852ad453f28d6c8e6fec
Abel Bodogan <bodoganabel@gmail.com>
2024-11-30 16:54:51 +0100

    Fixed out of bounds tooltip

--- frontend/functionality/tooltip/TooltipRoot.svelte
+++ frontend/functionality/tooltip/TooltipRoot.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+  import { onDestroy, onMount, tick } from "svelte";
   import { tooltipStore } from "./tooltip.store";
+  import type { Unsubscriber } from "svelte/store";
 
   function closeTooltip() {
     tooltipStore.set({ content: "", visible: false, x: 0, y: 0 });
@@ -11,20 +13,66 @@
 
   let content = "";
   let visible = false;
-  $: {
-    if (
-      typeof window !== "undefined" &&
-      typeof document !== "undefined" &&
-      tooltipRef !== undefined
-    ) {
-      content = $tooltipStore.content;
-      visible = $tooltipStore.visible;
-      console.log("tooltopStore from root:");
-      console.log($tooltipStore);
-      if ($tooltipStore.visible) {
-        document.addEventListener("click", closeTooltip);
-        tooltipRef.style.left = `${$tooltipStore.x}px`;
-        tooltipRef.style.top = `${$tooltipStore.y}px`;
+
+  let unsubscribe: Unsubscriber = () => {};
+  onMount(async () => {
+    unsubscribe = tooltipStore.subscribe(async () => {
+      if (
+        typeof window !== "undefined" &&
+        typeof document !== "undefined" &&
+        tooltipRef !== undefined
+      ) {
+        await calculateTooltipPosition();
+      }
+    });
+  });
+
+  onDestroy(() => {
+    unsubscribe();
+  });
+
+  async function calculateTooltipPosition() {
+    if ($tooltipStore.visible) {
+      document.addEventListener("click", closeTooltip);
+
+      // Calculate tooltip position
+      let tooltipX = $tooltipStore.x;
+      let tooltipY = $tooltipStore.y;
+
+      // Adjust position to keep the tooltip within the viewport
+      tooltipRef.style.left = `${tooltipX}px`;
+      tooltipRef.style.top = `${tooltipY}px`;
+
+      // Wait for the DOM to update
+      await tick();
+
+      // Calculate tooltip position if it goes out of viewport
+      const tooltipRect = tooltipRef.getBoundingClientRect();
+
+      const viewportWidth = window.innerWidth;
+      const viewportHeight = window.innerHeight;
+
+      let isModified = false;
+      if (tooltipRect.right > viewportWidth) {
+        tooltipX -= tooltipRect.right - viewportWidth;
+        isModified = true;
+      }
+      if (tooltipRect.bottom > viewportHeight) {
+        tooltipY -= tooltipRect.bottom - viewportHeight;
+        isModified = true;
+      }
+      if (tooltipRect.left < 0) {
+        tooltipX += Math.abs(tooltipRect.left);
+        isModified = true;
+      }
+      if (tooltipRect.top < 0) {
+        tooltipY += Math.abs(tooltipRect.top);
+        isModified = true;
+      }
+
+      if (isModified) {
+        tooltipRef.style.left = `${tooltipX}px`;
+        tooltipRef.style.top = `${tooltipY}px`;
       }
     }
   }
@@ -36,12 +84,12 @@
 <div
   bind:this={tooltipRef}
   id="tooltip-root"
-  class="card p-4 absolute z-20 variant-filled-primary transform -translate-x-1/2 -translate-y-full shadow-lg"
+  class="card p-4 absolute z-10 variant-filled-primary shadow-lg transform -translate-x-1/2 -translate-y-full"
   class:visible={$tooltipStore.visible}
   class:hidden={!$tooltipStore.visible}
   on:click|stopPropagation={() => {}}
 >
-  <p>{@html content}</p>
+  <p>{@html $tooltipStore.content}</p>
   <div class="arrow variant-filled-primary"></div>
 </div>
 
--- frontend/functionality/tooltip/tooltip.store.ts
+++ frontend/functionality/tooltip/tooltip.store.ts
@@ -13,26 +13,19 @@ export const tooltipStore = writable<{
 });
 
 export function tooltip(event: MouseEvent, content: string) {
-  console.log("listen");
-  console.log("content:");
-  console.log(content);
   if (typeof window === "undefined") return;
   if (typeof document === "undefined") return;
   try {
     // Temporarily hide the tooltip
     tooltipStore.set({ content: "", visible: false, x: 0, y: 0 });
 
-    const rect = (event?.target as any).getBoundingClientRect();
     const tooltipX = event.pageX;
     const tooltipY = event.pageY - 12;
 
+    /* This is a debouncer, otherwise the initiating click event will trigger the hiding mechanism of the tooltip */
     setTimeout(() => {
-      console.log(rect);
       tooltipStore.set({ content, visible: true, x: tooltipX, y: tooltipY });
     }, 10);
-
-    console.log("set");
-    console.log(get(tooltipStore));
   } catch (error) {
     console.warn(error);
   }
