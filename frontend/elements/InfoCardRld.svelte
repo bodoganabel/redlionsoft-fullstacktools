89da33de7145cbe01102ea6f84f91dac43cfd80d
Abel Bodogan <bodoganabel@gmail.com>
2024-12-11 01:10:58 +0100

    Added info card component

---
+++ frontend/elements/InfoCardRld.svelte
@@ -0,0 +1,16 @@
+<script>
+  import IconInfo from "../icons/IconInfo.svelte";
+
+  export let mt = "0";
+</script>
+
+<div class="mt-{mt} py-1 px-4 card bg-warning-300">
+  <div class="flex justify-stretch items-start space-x-2">
+    <div class="mt-[3px] min-w-5 w-5 text-gray-500">
+      <IconInfo />
+    </div>
+    <div class="flex flex-1 w-max text-gray-500">
+      <slot />
+    </div>
+  </div>
+</div>
