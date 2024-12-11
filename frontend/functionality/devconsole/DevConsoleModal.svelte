c0c9e924d9bbbc1b26f34ecbe99e790727d94dec
Abel Bodogan <bodoganabel@gmail.com>
2024-12-11 21:23:23 +0100

    Removed non-working Testmail from devtools

--- frontend/functionality/devconsole/DevConsoleModal.svelte
+++ frontend/functionality/devconsole/DevConsoleModal.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
   import type { IQuickloginUser } from "./devconsole.types";
   import Quicklogin from "./modules/Quicklogin.svelte";
-  import Testmail from "./modules/Testmail.svelte";
   export let quickloginUsers: IQuickloginUser[];
   export let templatePaths: string[];
 </script>
 
 <Quicklogin {quickloginUsers}></Quicklogin>
-<Testmail {templatePaths} />
