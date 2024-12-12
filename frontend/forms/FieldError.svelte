6bcf658d370ab72e3994aa56ee7b7d093a6a03f2
Abel Bodogan <bodoganabel@gmail.com>
2024-12-12 02:39:32 +0100

    Updated field error to accetp font changer classes

--- frontend/forms/FieldError.svelte
+++ frontend/forms/FieldError.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-	import { derived, type Writable } from 'svelte/store';
-	import type { IFormContext } from './form';
+  import { derived, type Writable } from "svelte/store";
+  import type { IFormContext } from "./form";
 
-	export let name: string;
-	export let formContext: IFormContext;
+  export let name: string;
+  export let formContext: IFormContext;
 
-	const { errors } = formContext;
+  const { errors } = formContext;
 
-	const errorMessages = derived(errors, ($errors) => $errors[name] || []);
+  const errorMessages = derived(errors, ($errors) => $errors[name] || []);
 </script>
 
 {#if $errorMessages.length}
-	<div class="error-messages">
-		{#each $errorMessages as error}
-			<p class="text-error-500">{@html error}</p>
-		{/each}
-	</div>
+  <div class="error-messages">
+    {#each $errorMessages as error}
+      <div class="text-error-500">{@html error}</div>
+    {/each}
+  </div>
 {/if}
