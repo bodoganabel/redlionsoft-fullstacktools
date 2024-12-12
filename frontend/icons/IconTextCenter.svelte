28f80e7fb0780031c334835f28a741465385b9b9
Abel Bodogan <bodoganabel@gmail.com>
2024-12-12 01:48:49 +0100

    Added icons

---
+++ frontend/icons/IconTextCenter.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let width = "100%";
+  export let height = "100%";
+</script>
+
+<svg
+  {width}
+  {height}
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke-width="1.5"
+  stroke="currentColor"
+  class="size-6"
+>
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+  />
+</svg>
