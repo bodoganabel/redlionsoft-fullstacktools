b026775a9407d3344aeac2fca5ffa34569a75714
Abel Bodogan <bodoganabel@gmail.com>
2024-12-16 23:58:02 +0100

    Added uuidShort

--- common/utilities/general.ts
+++ common/utilities/general.ts
@@ -1,16 +1,21 @@
 import { isProduction } from "..";
+import ShortUniqueId from "short-unique-id";
 
 export function delay(ms: number): Promise<void> {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export function uuidShort(length = 10) {
+  return new ShortUniqueId({ length }).rnd();
 }
 
 export function UuidSimple() {
-    const time = new Date().getTime();
-    return time.toString();
+  const time = new Date().getTime();
+  return time.toString();
 }
 
 export function devOnly(callOnlyAtDevelopmentMode: () => any) {
-    if (!isProduction()) {
-        callOnlyAtDevelopmentMode();
-    }
+  if (!isProduction()) {
+    callOnlyAtDevelopmentMode();
+  }
 }
