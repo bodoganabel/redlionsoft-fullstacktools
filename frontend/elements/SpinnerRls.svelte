22e001dce2ecb865c6ddfcb9e66e091a95f21764
Abel Bodogan <bodoganabel@gmail.com>
2024-12-27 02:12:27 +0100

    Spinner upgrade

--- frontend/elements/SpinnerRls.svelte
+++ frontend/elements/SpinnerRls.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { ProgressRadial } from "@skeletonlabs/skeleton";
   export let size = "w-full";
-  export let stroke = 10;
+  export let stroke = 40;
   export let meter = "stroke-primary-500";
   export let track = "stroke-primary-500/30";
 </script>
