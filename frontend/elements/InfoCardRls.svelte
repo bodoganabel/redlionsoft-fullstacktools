73a34f0486aadef3cfcaaa1d575c0fba8852c190
Abel Bodogan <bodoganabel@gmail.com>
2025-02-24 15:16:34 +0100

    decoupled infoCard from card

--- frontend/elements/InfoCardRls.svelte
+++ frontend/elements/InfoCardRls.svelte
@@ -4,7 +4,9 @@
   export let mt = "0";
 </script>
 
-<div class="mt-{mt} py-1 px-4 card bg-warning-300">
+<div
+  class="mt-{mt} py-2 border border-1 border-gray-300 dark:border-gray-600 px-4 rounded-md bg-warning-300"
+>
   <div class="flex justify-stretch items-start space-x-2">
     <div class="mt-[3px] min-w-5 w-5 text-gray-500">
       <IconInfo />
