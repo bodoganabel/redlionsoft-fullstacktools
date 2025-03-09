1c2895d72b3c58ae6fe1f6aa6a1ca4fa52153c17
Abel Bodogan <bodoganabel@gmail.com>
2025-03-10 00:56:54 +0100

    Pretttify

--- common/index.ts
+++ common/index.ts
@@ -1,12 +1,11 @@
-export * from './validators/validators';
-
+export * from "./validators/validators";
 
 /**
  * Determines if the application is running in production mode by process.env.NODE_ENV === 'production'.
- * 
+ *
  * @returns {boolean} - True if the application is running in production mode, false otherwise.
  */
 export const isProduction = (): boolean => {
-    // Check if the environment variable is defined and is set to 'production'.
-    return process.env.NODE_ENV === 'production';
+  // Check if the environment variable is defined and is set to 'production'.
+  return process.env.NODE_ENV === "production";
 };
