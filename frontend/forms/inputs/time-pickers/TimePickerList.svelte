2878955878660bc5599e10ecd6141fbe9f81d9dc
Abel Bodogan <bodoganabel@gmail.com>
2025-05-19 01:18:05 +0200

    Added confirmation

--- frontend/forms/inputs/time-pickers/TimePickerList.svelte
+++ frontend/forms/inputs/time-pickers/TimePickerList.svelte
@@ -1,20 +1,35 @@
 <script lang="ts">
-  import { DateTime } from "luxon";
+  import { DateTime } from 'luxon';
   export let availableSlots: DateTime[];
   export let format = DateTime.TIME_24_SIMPLE;
   export let onSelect: (date: DateTime) => void = () => {};
   export let selectedDateTime: DateTime | null = null;
+  export let onConfirmSelection: (date: DateTime) => void = () => {};
+
+  let selectedIndex = -1;
 </script>
 
 {#if !availableSlots || availableSlots.length === 0}
   <p>-</p>
 {:else}
-  {#each availableSlots as availableDateTime}
-    <button
-      class={selectedDateTime?.toISO() === availableDateTime.toISO() ? 'variant-filled-primary' : 'variant-outline-primary'}
-      on:click={async () => {
-        onSelect(availableDateTime);
-      }}>{availableDateTime.toLocaleString(format)}</button
-    >
+  {#each availableSlots as availableDateTime, index}
+    <div class="w-full flex justify-center items-center" class:gap-2={selectedIndex === index}>
+      <button
+        class={`${selectedIndex === index ? 'variant-glass-primary' : 'variant-outline-primary'} flex-1`}
+        on:click={async () => {
+          onSelect(availableDateTime);
+          selectedIndex = index;
+        }}>{availableDateTime.toLocaleString(format)}</button
+      >
+      {#if selectedIndex === index}
+        <button
+          class="variant-filled-primary"
+          on:click={async () => {
+            onConfirmSelection(availableDateTime);
+            selectedIndex = index;
+          }}>Confirm</button
+        >
+      {/if}
+    </div>
   {/each}
 {/if}
