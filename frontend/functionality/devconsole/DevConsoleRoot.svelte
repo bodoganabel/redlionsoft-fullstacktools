044c341aac70f93876578428413c6ce2d6c2e9d1
Abel Bodogan <bodoganabel@gmail.com>
2025-06-08 00:18:03 +0200

    No more devconsole in production mode

--- frontend/functionality/devconsole/DevConsoleRoot.svelte
+++ frontend/functionality/devconsole/DevConsoleRoot.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
-  import DevConsoleModal from "./DevConsoleModal.svelte";
-  import IconCode from "../../icons/IconCode.svelte";
-  import { isProduction } from "../../../common/index";
-  import type { IQuickloginUser } from "./devconsole.types";
-  import { popup } from "../popup/popup-logic";
-  import { onDestroy, onMount } from "svelte";
+  import DevConsoleModal from './DevConsoleModal.svelte';
+  import IconCode from '../../icons/IconCode.svelte';
+  import { isProduction } from '../../../common/index';
+  import type { IQuickloginUser } from './devconsole.types';
+  import { popup } from '../popup/popup-logic';
+  import { onDestroy, onMount } from 'svelte';
 
   export let quickloginUsers: IQuickloginUser[];
 
   const isDevelopment = !isProduction();
 
   const cmdDListener = (event: any) => {
-    const isWindows = navigator.platform.indexOf("Win") !== -1;
-    const isCorrectKey = event.key === "d";
+    if (isDevelopment) return;
+    const isWindows = navigator.platform.indexOf('Win') !== -1;
+    const isCorrectKey = event.key === 'd';
     const isCorrectModifier = isWindows ? event.altKey : event.metaKey;
     if (isCorrectKey && isCorrectModifier) {
       event.preventDefault();
@@ -21,20 +22,20 @@
   };
 
   onMount(() => {
-    if (typeof window !== "undefined") {
-      document.addEventListener("keydown", cmdDListener);
+    if (typeof window !== 'undefined') {
+      document.addEventListener('keydown', cmdDListener);
     }
   });
 
   onDestroy(() => {
-    if (typeof window !== "undefined") {
-      document.removeEventListener("keydown", cmdDListener);
+    if (typeof window !== 'undefined') {
+      document.removeEventListener('keydown', cmdDListener);
     }
   });
 
   function toggleDevtools() {
     popup({
-      title: "Devconsole",
+      title: 'Devconsole',
       component: DevConsoleModal,
       componentProps: { quickloginUsers },
     });
@@ -43,10 +44,7 @@
 
 {#if isDevelopment}
   <div class="fixed bottom-2 left-2 z-50">
-    <button
-      class="icon variant-outline-warning"
-      on:click={() => toggleDevtools()}
-    >
+    <button class="icon variant-outline-warning" on:click={() => toggleDevtools()}>
       <IconCode />
     </button>
   </div>
