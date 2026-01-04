0406399c06da279b49d2eec438b07d9dffcd7efa
Abel Bodogan <bodoganabel@gmail.com>
2026-01-04 14:58:51 +0100

    Fixed PUT template

--- backend/endpoints/templates/epput/dto.ts
+++ backend/endpoints/templates/epput/dto.ts
@@ -2,17 +2,17 @@
 
 import z from "zod/v4";
 
-export const SNamexxx_DELETE_Body = z.object({
-  inDELETE: z.string(),
+export const SNamexxx_PUT_Body = z.object({
+  input: z.string(),
 });
-export type TNamexxx_DELETE_Body = z.infer<typeof SNamexxx_DELETE_Body>
+export type TNamexxx_PUT_Body = z.infer<typeof SNamexxx_PUT_Body>
 
-export const SNamexxx_DELETE_Response = z.object({
-  outDELETE: z.string(),
+export const SNamexxx_PUT_Response = z.object({
+  output: z.string(),
 })
-export type TNamexxx_DELETE_Response = z.infer<typeof SNamexxx_DELETE_Response>
+export type TNamexxx_PUT_Response = z.infer<typeof SNamexxx_PUT_Response>
 
-export enum ENamexxx_DELETE_ErrorCodes {
+export enum ENamexxx_PUT_ErrorCodes {
     ID_NOT_FOUND = "ID_NOT_FOUND",
     AUTO_TOAST = "AUTO_TOAST"
 }
