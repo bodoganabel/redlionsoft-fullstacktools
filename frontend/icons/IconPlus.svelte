bc0e408396089f6d9b55a6a4b940b049bc8a858f
Abel Bodogan <bodoganabel@gmail.com>
2026-01-04 16:35:25 +0100

    fixed iconplus

--- frontend/icons/IconPlus.svelte
+++ frontend/icons/IconPlus.svelte
@@ -1,10 +1,17 @@
+<script lang="ts">
+	export let width = "100%";
+	export let height = "100%";
+	
+</script>
+
 <svg
 	xmlns="http://www.w3.org/2000/svg"
 	fill="none"
 	viewBox="0 0 24 24"
 	stroke-width="1.5"
 	stroke="currentColor"
-	class="w-6 h-6"
+	{width}
+	{height}
 >
 	<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
 </svg>
