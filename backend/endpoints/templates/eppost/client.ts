0dff72ea878ddaab7f118a961b4795663a5bf935
Abel Bodogan <bodoganabel@gmail.com>
2026-01-04 19:05:33 +0100

    fixed post

--- backend/endpoints/templates/eppost/client.ts
+++ backend/endpoints/templates/eppost/client.ts
@@ -1,12 +1,13 @@
 import { apiRequest } from "$redlionsoft/frontend/client/api-request";
 import { toastError } from "$redlionsoft/frontend/functionality/toast/toast-logic";
 import { ENamexxx_POST_ErrorCodes, type TNamexxx_POST_Body, type TNamexxx_POST_Response } from "./dto";
+import { clientBase } from "$src/client.base";
 
 
 export async function clientRequestNamexxx_POST(body: TNamexxx_POST_Body): Promise<any | null> {
 
     const { data, error } = await apiRequest<TNamexxx_POST_Response, TNamexxx_POST_Body>({
-        url: `/events/input/api`,
+        url: clientBase.automaticApiUrl(import.meta.url),
         method: "POST",
         body
     });
